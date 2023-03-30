355d548db11d17eae6daa15b870904d38a84862b
Harshid Vaddoriya <harshidpatel388@gmail.com>
2023-03-30 10:01:12 +0530

    complete

--- Get API program/ViewController.swift
+++ Get API program/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 struct GetApi: Codable{
     var postId: Int
@@ -19,7 +20,7 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     
     
     var arr : [GetApi] = []
-
+    
     @IBOutlet weak var tableData: UITableView!
     
     override func viewDidLoad() {
@@ -28,44 +29,67 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
         getData()
     }
     
+    //    func getData(){
+    //        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1/comments")
+    //        var ur = URLRequest(url: url!)
+    //        ur.httpMethod = "GET"
+    //
+    //        URLSession.shared.dataTask(with: ur) { data, response, error in
+    //            do{
+    //                if error == nil{
+    //                    self.arr = try JSONDecoder().decode([GetApi].self, from: data!)
+    //                    //print(self.arr)
+    //                    DispatchQueue.main.async {
+    //                        self.tableData.reloadData()
+    //                    }
+    //                }
+    //            }
+    //            catch{
+    //                print(error.localizedDescription)
+    //            }
+    //        }.resume()
+    //    }
+    
     func getData(){
-        let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1/comments")
-        var ur = URLRequest(url: url!)
-        ur.httpMethod = "GET"
-        
-        URLSession.shared.dataTask(with: ur) { data, response, error in
-            do{
-                if error == nil{
-                    self.arr = try JSONDecoder().decode([GetApi].self, from: data!)
-                    //print(self.arr)
-                    DispatchQueue.main.async {
-                        self.tableData.reloadData()
+        AF.request("https://jsonplaceholder.typicode.com/posts/1/comments",method: .get).responseData { respo in
+            switch respo.result{
+            case.success(let data):
+                do{
+                    if data == respo.value{
+                        self.arr = try JSONDecoder().decode([GetApi].self, from: data)
+                        //print(self.arr)
+                        DispatchQueue.main.async {
+                            self.tableData.reloadData()
+                        }
                     }
                 }
+                catch{
+                    print(error.localizedDescription)
+                }
+            case.failure(let err):
+                print(err.localizedDescription)
             }
-            catch{
-                print(error.localizedDescription)
-            }
-        }.resume()
+        }
+            
     }
+
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return arr.count
+}
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyTableViewCell
+    cell.lb1.text = "\(arr[indexPath.row].id)"
+    cell.lb2.text = arr[indexPath.row].email
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arr.count
-    }
+    return cell
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyTableViewCell
-        cell.lb1.text = "\(arr[indexPath.row].id)"
-        cell.lb2.text = arr[indexPath.row].email
-        
-        return cell
-        
-    }
-    
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150
-    }
+}
+
+
+func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 150
+}
 
 }
 
